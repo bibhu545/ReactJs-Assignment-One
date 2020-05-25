0e4965d21593e99bdba2151e065cffe219901562
bibhu545 <bibhuranjan.545@gmail.com>
2020-05-25 10:54:30 +0530

    Removed context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home';
-import { CommonProvider } from "./Utils/CommonContext";
 
 function App() {
   return (
-    <CommonProvider>
       <Home />
-    </CommonProvider>
   );
 }
 
